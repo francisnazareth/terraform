5e55929bd8cdcf80f3049a2bad3ddbdc1573cceb
Francis Simy Nazareth <simy@outlook.com>
2021-07-15 10:50:29 +0300

    changing to module structure

--- la_workspace.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_log_analytics_workspace" "la-laworksplace-hub" {
-  name                = "laworkspace-hub-001"
-  location            = var.hub-location
-  resource_group_name = var.hub-rg
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
--- recovery-svc-vault.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_recovery_services_vault" "vault" {
-  name                = "rsv-hub"
-  location            = var.hub-location
-  resource_group_name = var.hub-rg
-  sku                 = "Standard"
-
-  soft_delete_enabled = true
-}
