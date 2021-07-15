c9d82883cd0e5ba99328889f6ed52be3ab4ee680
francisnazareth <73844173+francisnazareth@users.noreply.github.com>
2021-07-15 10:56:46 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,36 +23,43 @@ variable "hub-vnet-address-space" {
   default = "10.105.0.0/22"
 }
 
-variable "gateway-subnet-address-space" {
-  description = "Gateway subnet address space"
-  default = "10.105.0.128/27"
-}
-
-variable "bastion-subnet-address-space" {
-  description = "Bastion subnet address space"
-  default = "10.105.0.160/27"
-}
-
+#Address range for the Firewall subnet (1-63)
 variable "firewall-subnet-address-space" {
   description = "Firewall subnet address space"
   default = "10.105.0.0/26"
 }
 
+#Address range for the application gateway subnet (64 - 127)
 variable "appgw-subnet-address-space" {
   description = "Application gateway subnet address space"
   default = "10.105.0.64/26"
 }
 
+#Address range for the VNET gateway subnet (128 - 159)
+variable "gateway-subnet-address-space" {
+  description = "Gateway subnet address space"
+  default = "10.105.0.128/27"
+}
+
+#Address range for the bastion subnet (160 - 191)
+variable "bastion-subnet-address-space" {
+  description = "Bastion subnet address space"
+  default = "10.105.0.160/27"
+}
+
+#Address range for the management subnet 1 (192 - 207)
 variable "mgmt-subnet-1-address-space" {
   description = "Management subnet 1 address space"
   default = "10.105.0.192/28"
 }
 
+#Address range for the management subnet 2 (208 - 223)
 variable "mgmt-subnet-2-address-space" {
   description = "Management subnet 2 address space"
   default = "10.105.0.208/28"
 }
 
+#Address range for the shared services subnet (224 - 239)
 variable "shared-svcs-snet-address-space" {
   description = "Shared services subnet address space"
   default = "10.105.0.224/28"
