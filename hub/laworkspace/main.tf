f39dba70890b88bdacfa2325e8791e3ee956448b
Ubuntu <francis@fn-ubuntu.brjqqan0vxqejeun0qhfv0leqe.ax.internal.cloudapp.net>
2021-07-27 10:04:59 +0000

    la-workspace

--- hub/laworkspace/main.tf
+++ hub/laworkspace/main.tf
@@ -5,3 +5,17 @@ resource "azurerm_log_analytics_workspace" "la-workspace-hub" {
   sku                 = "PerGB2018"
   retention_in_days   = var.la-log-retention-in-days
 }
+
+resource "azurerm_log_analytics_solution" "la-hub-solution" {
+  solution_name         = "ContainerInsights"
+  location              = var.rg-location
+  resource_group_name   = var.rg-name
+  workspace_resource_id = azurerm_log_analytics_workspace.la-workspace-hub.id
+  workspace_name        = azurerm_log_analytics_workspace.la-workspace-hub.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
+}
+
